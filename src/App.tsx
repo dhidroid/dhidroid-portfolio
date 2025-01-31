5797ef7b9067484f5c07406447ca244761e10269
dhinesh4668nato <dhinesh@natobotics.com>
2025-01-31 22:27:09 +0530

    add: sitemap, robots.txt, meta data add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import type { Component } from 'solid-js';
+import { createEffect, type Component } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+import { Link, Meta, MetaProvider, Title } from "@solidjs/meta";
+import { logEvent, analytics } from "../firebaseconfig";
+import { useLocation } from "@solidjs/router";
+const trackPageView = () => {
+  const location = useLocation();
 
+  createEffect(() => {
+    logEvent(analytics, "page_view", {
+      page_path: location.pathname,
+    });
+  });
+};
 const App: Component = () => {
   return (
     <div class={styles.App}>
+      <MetaProvider>
+        <Title>dhidroid | Home</Title>
+        <Meta name="description" content="my portfolio" />
+        <Link rel="canonical" href="https://dhidroi.web.app" />
+      </MetaProvider>
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
         <p>
