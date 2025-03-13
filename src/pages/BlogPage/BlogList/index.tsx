682cf1702873d7304968f096a5bb2c8880c2824b
Dhineshkumar Thirupathi <87540180+dhidroid@users.noreply.github.com>
2025-03-13 03:11:29 +0000

    url rewrite

--- src/pages/BlogPage/BlogList/index.tsx
+++ src/pages/BlogPage/BlogList/index.tsx
@@ -1,7 +1,57 @@
+import React from 'react'
+import Loader from '../../../components/loader/Loader'
+import style from './index.module.css'
+import { client } from '../../../senity/senity'
+import { GoDotFill } from 'react-icons/go'
+
 const BlogList = () => {
+    const [loading, setLoading] = React.useState(false)
+    const [category, setCategory] = React.useState([])
+    const [blogData, setBlogData] = React.useState([])
+
+    React.useEffect(() => {
+        fetchCatagory();
+        fetchBlogData();
+    }, [])
+
+    const fetchCatagory = async () => {
+        setLoading(true)
+        try {
+            const fetchData = await client.fetch(`*[_type == "category"]`);
+            setCategory(fetchData)
+        } catch (err) {
+            console.log(err)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const fetchBlogData = async () => {
+        setLoading(true)
+        try {
+            const fetchData = await client.fetch(`*[_type == "post"]`);
+            setBlogData(fetchData);
+            console.log(fetchData)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+    if (loading) {
+        return <Loader />
+    }
     return (
-        <div>
-            <h1>blog list</h1>
+        <div className={style.container}>
+            {/* title */}
+            <div className={style.titleContainer}>
+                <h1>Blogs List</h1>
+                <p>Home
+                    <GoDotFill />
+                    <span>My Blog List</span></p>
+            </div>
+
+            <div className={style.innerContainer}></div>
         </div>
     )
 }
