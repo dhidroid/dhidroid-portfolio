f40cd86074ced891aa53d98cba2e42a6d47df5c0
dhinesh4668 <dhinesh4668@gmail.com>
2025-05-27 14:05:06 +0000

    new dev v1 version

--- src/router/rootNavigation.tsx
+++ src/router/rootNavigation.tsx
@@ -1,13 +1,10 @@
 import React, { Suspense } from "react";
 import { Route, Routes } from "react-router";
-import Loader from "../components/loader/Loader";
-import Header from "../components/header/Header";
-import Footer from "../components/Footer/Footer";
-import PageNotFound from "../components/404Page/PageNotFound";
 import { AboutPage, BlogPage } from "../pages";
 import UndertheDev from "../components/404Page/UndertheDev";
 import Projects from "../pages/Projects";
 import BlogList from "../pages/BlogPage/BlogList";
+import { Footer, Header, Loader, PageNotFound } from "../components";
 
 // Lazy load the HomePage component
 const HomeScreen = React.lazy(() => import("../pages/HeroPage/index"));
@@ -32,7 +29,7 @@ export const Router = () => {
         <Header />
         <Routes>
           <Route path="/" element={<HomeScreen />} />
-          {/* <Route path="*" element={<Suspense fallback={<Loader />}><UndertheDev /></Suspense>} /> */}
+          <Route path="*" element={<Suspense fallback={<Loader />}><UndertheDev /></Suspense>} />
           <Route path="/blog/:slug" element={<BlogPage />} />
           <Route path="/createblog" element={<BlogCreate />} />
           <Route path="/project" element={<Projects />} />
