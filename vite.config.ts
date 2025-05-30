b51efd06b7f720cf01abc0a80a44e58357d48599
dhinesh4668 <dhinesh4668@gmail.com>
2025-05-30 08:16:45 +0000

    module css add

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      // Enable CSS modules for files ending with .module.css
+      localsConvention: 'camelCaseOnly', // or 'camelCase', 'dashes', etc.
+      generateScopedName: '[name]__[local]___[hash:base64:5]'
+    }
+  },
   resolve: {
     alias: {
       '@Screens': path.resolve(__dirname, 'src/pages/index.ts'),
