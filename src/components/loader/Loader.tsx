6f8495c53b39df0bf15eb1aa34dcd0b694e4380e
dhinesh4668 <dhinesh4668@gmail.com>
2025-10-28 17:37:10 +0000

    loader mobile view fix

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,6 +1,5 @@
 import Lottie from "lottie-react";
 import React from "react";
-// import { InfinitySpin } from "react-loader-spinner";
 import { LoaderAnimation } from "../../assets";
 
 const Loader = (): React.JSX.Element => {
@@ -11,24 +10,36 @@ const Loader = (): React.JSX.Element => {
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
+        width: "100vw",
+        padding: "20px",
+        boxSizing: "border-box",
       }}
     >
-      {/* <InfinitySpin width="200" color="#5315FC" /> */}
-      <center>
-      {/* <Loader /> */}
-      <div style={{      
-      }}>
-      <Lottie animationData={LoaderAnimation}  style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "20%",
-        padding: 40,
-        backgroundColor: "white",
-        borderRadius: 100
-      }} loop={true}/>
-</div>
-    </center>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "min(300px, 80vw)",
+          height: "min(300px, 80vw)",
+          maxWidth: "400px",
+          maxHeight: "400px",
+          padding: "clamp(20px, 5vw, 40px)",
+          backgroundColor: "white",
+          borderRadius: "50%",
+          boxShadow: "0 4px 20px rgba(0, 0, 0, 0.1)",
+          boxSizing: "border-box",
+        }}
+      >
+        <Lottie
+          animationData={LoaderAnimation}
+          style={{
+            width: "100%",
+            height: "100%",
+          }}
+          loop={true}
+        />
+      </div>
     </div>
   );
 };
