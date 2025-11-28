cf0d1e9e93bc6251218108e9121de061ddf9e00e
Dhineshkumar Thirupathi <87540180+dhidroid@users.noreply.github.com>
2025-11-28 17:50:18 +0000

    chore: Update comment formatting in vite.config.ts for clarity

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vite.dev/config/ 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
