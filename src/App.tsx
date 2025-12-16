437e396979037f6401e92516c14ba7bb21444cd1
Dhineshkumar Thirupathi <87540180+dhidroid@users.noreply.github.com>
2025-12-16 17:41:39 +0000

    Add @vercel/speed-insights dependency and integrate into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Router } from "./router/rootNavigation";
 import { Analytics } from "@vercel/analytics/react"
 import ChatBot from "./components/ChatBot/ChatBot";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { AIProvider } from "./context/AIContext";
 
 const App = () => {
   return (
     <AIProvider>
+      <SpeedInsights />
       <Router />
       <ChatBot />
       <Analytics />
