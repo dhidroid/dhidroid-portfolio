f4a031e5886cc1a6bc50972523120155365e50d9
Dhineshkumar Thirupathi <87540180+dhidroid@users.noreply.github.com>
2025-12-16 17:42:49 +0000

    Fix import statement for SpeedInsights component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Router } from "./router/rootNavigation";
 import { Analytics } from "@vercel/analytics/react"
 import ChatBot from "./components/ChatBot/ChatBot";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { AIProvider } from "./context/AIContext";
 
 const App = () => {
